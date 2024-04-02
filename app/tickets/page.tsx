43d46ff5124799fbe8dc11166241f0fe1341b52b
Can Zhao <canzhao216@gmail.com>
2024-04-01 21:36:04 -0500

    add ticket form

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import prisma from "@/prisma/db";
+import DataTable from "./DataTable";
 
-const Tickets = () => {
-  return <div>Tickets</div>;
+const Tickets = async () => {
+  const tickets = await prisma.tickets.findMany();
+  console.log("Hello World");
+  return (
+    <div>
+      <DataTable tickets={tickets} />
+    </div>
+  );
 };
 
 export default Tickets;
